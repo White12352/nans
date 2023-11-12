3e2e7d4e0f57839fe21122e958066b0735963d41
White12352 <86940955+White12352@users.noreply.github.com>
2023-11-12 16:25:22 +0800

    Update core.sh

--- buildScript/lib/core.sh
+++ buildScript/lib/core.sh
@@ -8,7 +8,7 @@ rm -rf sing-box
 git clone -b dev-next https://github.com/SagerNet/sing-box.git sing-box
 git clone -b dev https://github.com/White12352/sing sing
 svn co https://github.com/MatsuriDayo/sing-box/branches/1.6.a2/nekoutils sing-box/nekoutils
-awk '/^replace/ && !found {print "replace github.com/sagernet/sing => ../sing"; found=1} 1' sing-box-extra/go.mod > go.mod.tmp && mv -f go.mod.tmp sing-box-extra/go.mod
+#awk '/^replace/ && !found {print "replace github.com/sagernet/sing => ../sing"; found=1} 1' sing-box-extra/go.mod > go.mod.tmp && mv -f go.mod.tmp sing-box-extra/go.mod
 cd sing-box-extra
 go mod tidy
 cd ../sing-box
@@ -47,8 +47,8 @@ sed -i '/berty\.tech\/go-libtor v[0-999]\+\.[0-999]\+\.[0-999]\+ \/\//a \\tgithu
 awk '/ProtocolSTUN = "stun"/ {print; print "\tProtocolBittorrent = \"bittorrent\""; inserted=1; next} 1; END {if (!inserted) print "\tProtocolBittorrent = \"bittorrent\""}' sing-box/constant/protocol.go > temp_file && mv -f temp_file sing-box/constant/protocol.go 
 awk '{ if (/sniffMetadata, err := sniff.PeekStream\(ctx, conn, buffer, time\.Duration\(metadata\.InboundOptions\.SniffTimeout\), sniff\.StreamDomainNameQuery, sniff\.TLSClientHello, sniff\.HTTPHost\)/) sub("sniff\\.HTTPHost", "sniff.HTTPHost, sniff.BittorrentTCPMessage") } { print }' sing-box/route/router.go > temp && mv -f temp sing-box/route/router.go 
 sed -i 's/sniffMetadata, _ := sniff.PeekPacket(ctx, buffer.Bytes(), sniff.DomainNameQuery, sniff.QUICClientHello, sniff.STUNMessage)/sniffMetadata, _ := sniff.PeekPacket(ctx, buffer.Bytes(), sniff.DomainNameQuery, sniff.QUICClientHello, sniff.STUNMessage, sniff.BittorrentUDPMessage)/' sing-box/route/router.go
-awk '{if(index($0, "//replace github.com/sagernet/sing") > 0) $0 = "replace github.com/sagernet/sing v0.2.18-0.20231108041402-4fbbd193203c => github.com/White12352/sing v0.2.19"}1' sing-box/go.mod > temp_file && mv -f temp_file sing-box/go.mod
-awk '/^replace/ && !found {print "replace github.com/sagernet/sing v0.2.18-0.20231108041402-4fbbd193203c => github.com/White12352/sing v0.2.19"; found=1} 1' sing-box/test/go.mod > go.mod.tmp && mv -f go.mod.tmp sing-box/test/go.mod
+awk '{if(index($0, "//replace github.com/sagernet/sing") > 0) $0 = "replace github.com/sagernet/sing => ../sing"}1' sing-box/go.mod > temp_file && mv -f temp_file sing-box/go.mod
+awk '/^replace/ && !found {print "replace github.com/sagernet/sing => ../../sing"; found=1} 1' sing-box/test/go.mod > go.mod.tmp && mv -f go.mod.tmp sing-box/test/go.mod
 #awk '/^replace/ && !found {print "replace github.com/sagernet/sing => ../../../../sing"; found=1} 1' nans/libcore/.build/src/go.mod > go.mod.tmp && mv -f go.mod.tmp nans/libcore/.build/src/go.mod
 awk '/^replace/ && !found {print "replace github.com/sagernet/sing => ../../sing"; found=1} 1' nans/libcore/go.mod > go.mod.tmp && mv -f go.mod.tmp nans/libcore/go.mod
 git clone -b dev https://github.com/SagerNet/sing-quic sing-quic
